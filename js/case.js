a5c454fb6b9d50c504c4545394dbbb79ff2d4df1
Tmac-1 <1906526613@qq.com>
2019-09-10 01:28:02 +0800

    添加微信分享

--- js/case.js
+++ js/case.js
@@ -22,9 +22,29 @@ $(function(){
         }
     })
     $('#closePage').on('click',function(){
-        console.log(111)
+        // console.log(111)
         window.opener=null;
 		window.open('','_self');
 		window.close();
+    })
+    var qrcode = new QRCode($('#qrcode')[0], {
+        text: window.location.href,
+        width: 128,
+        height: 128,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+    $('#share').on('click',function(){
+        layer = layui.layer
+        layer.open({
+            type: 1,
+            shade: false,
+            title: false, //不显示标题
+            content: $('.out'), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相对元素所影响
+            // cancel: function(){
+            //   layer.msg('捕获就是从页面已经存在的元素上，包裹layer的结构', {time: 5000, icon:6});
+            // }
+        });
     })
 })
