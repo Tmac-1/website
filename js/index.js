96404731639cae15cff4183e7bb39588565bb447
binbin.ren <binbin.ren@zouchuqu.com>
2019-09-10 22:00:10 +0800

    完善

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
 $(function(){
-    $('.close').on('click',function(){
-        $('.nav_warp').css('height',0)
-    })
-    $('#menu').on('click',function(){
-        $('.nav_warp').css('height',"100%")
-    })
-    $('.close').hover(function(){
-        $(this).css('transform',' rotate(180deg)')
-    },function(){
-        $(this).css('transform',' rotate(0deg)')
-    })
     $('.ul01').css('min-height', $('.imgItem').height())
     // console.log($(window).width())
     // 导航点击
@@ -31,16 +20,5 @@ $(function(){
     })
     $('.lookMore_p1').on('click',function(){
         layer.msg('没有更多案例了~')
-    })
-    $(window).scroll(function() {
-        //为了保证兼容性，这里取两个值，哪个有值取哪一个
-        //scrollTop就是触发滚轮事件时滚轮的高度
-        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        // console.log("滚动距离" + scrollTop);
-        if(scrollTop > 400){
-            $('.header_wrap').addClass('scroll_active')
-        }else{
-            $('.header_wrap').removeClass('scroll_active')
-        }
     })
 })
